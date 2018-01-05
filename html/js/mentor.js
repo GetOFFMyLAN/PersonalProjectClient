e1c4870ace0f4640f9574e916c15bef4fc70e2c6
John <axel.ramone@gmail.com>
2018-01-05 00:00:38 -0600

    commented out uneeded js (so basically this file is uneeded in its current state)

--- html/js/mentor.js
+++ html/js/mentor.js
@@ -3,7 +3,7 @@
 var mentors = 0;
 
 if (mentors == 0) {
-  document.getElementById('new-mess-btn').innerHTML = "You dont seem to have any mentors. Get started by clicking here";
+  // document.getElementById('new-mess-btn').innerHTML = "<style>.new-mess-btn {border-radius: 50%; width: 50px; height: 50px; border: 2px dashed #262626; margin-left: 20px; margin-top: 20px; position: relative;} </style>";
 
 } else {
 
